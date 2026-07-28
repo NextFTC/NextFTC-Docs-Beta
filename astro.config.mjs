07f01be6d35f9df06a90bded63cc5a5ff15cae5b
AchintyaAkula <achintya.akula@gmail.com>
2026-07-27 19:52:58 -0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Installation Guide', slug: 'guides/installation' },
 					],
 				},
 				{
